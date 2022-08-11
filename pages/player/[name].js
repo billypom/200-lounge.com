25b83fd4aa60d0ac6d0c4db2a0aef29423891867
billy pompey <billypompey@icloud.com>
2022-08-11 09:39:48 -0400

    stuff

--- pages/player/[name].js
+++ pages/player/[name].js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import mysql from 'mysql2'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 import { useRouter } from 'next/router'
 
 
