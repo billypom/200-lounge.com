ece634ee0bb4ad107e5116f2e14f9790b21d9045
billy pompey <billypompey@icloud.com>
2022-09-02 17:23:13 -0400

    fix columns

--- pages/index.js
+++ pages/index.js
@@ -343,7 +343,7 @@ export default function Leaderboard({ rows }) {
                       ? sort(filter(rows)).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage):sort(filter(rows))).map((row, idx) => (
                         isMobile ? 
                           <StyledTableRow key={row.player_id}>
-                            <StyledTableCell align="center text-primary">
+                            <StyledTableCell align="center">
                               <div className={row.MMR >= 11000 ? 'text-red-800' : row.MMR >= 9000 ? 'text-violet-700' : row.MMR >= 7500 ? 'text-cyan-200' : row.MMR >= 6000 ? 'text-cyan-600' : row.MMR >= 4500 ? 'text-yellow-500' : row.MMR >= 3000 ? 'text-gray-400' : row.MMR >= 1500 ? 'text-orange-400' : 'text-stone-500'}>
                                   <div className='cursor-pointer hover:underline'>
                                   <Link href={"/player/" + row['Player Name']}>
@@ -353,9 +353,9 @@ export default function Leaderboard({ rows }) {
                               </div>
                             </StyledTableCell>
                             
-                            <StyledTableCell align="center">
+                            {/* <StyledTableCell align="center">
                               <ReactCountryFlag countryCode={row.Country} style={{width: '2rem', height: '2rem'}} svg />
-                            </StyledTableCell>
+                            </StyledTableCell> */}
 
                             <StyledTableCell align="center">
                               <div className={row.MMR >= 11000 ? 'text-red-800' : row.MMR >= 9000 ? 'text-violet-700' : row.MMR >= 7500 ? 'text-cyan-200' : row.MMR >= 6000 ? 'text-cyan-600' : row.MMR >= 4500 ? 'text-yellow-500' : row.MMR >= 3000 ? 'text-gray-400' : row.MMR >= 1500 ? 'text-orange-400' : 'text-stone-500'}>
